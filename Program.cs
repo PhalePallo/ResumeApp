9cf33f4ef35fe56e1b1df5f565dd59917a249eaf
PhalePallo <160241510+PhalePallo@users.noreply.github.com>
2025-03-12 18:57:29 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -17,7 +17,7 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
 
-// Set up default routing
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Resume}/{action=Index}/{id?}");
